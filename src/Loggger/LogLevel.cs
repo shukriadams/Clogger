507c4d065b1e07fdb8fbde1ed4f42f3c38fcf031
Shukri Adams <mail@shukriadams.com>
2026-09-24 13:31:09 +0200

    Create LogLevel.cs

---
+++ src/Loggger/LogLevel.cs
@@ -0,0 +1,20 @@
+namespace Madscience.Logging
+{
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Logs Warn and Error. Default.
+        /// </summary>
+        Warn, 
+
+        /// <summary>
+        /// Logs Debug, Warn and Error.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// Logs Trace, Debug, Warn and Error.
+        /// </summary>
+        Trace
+    }
+}
