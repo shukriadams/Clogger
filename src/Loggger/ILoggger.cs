d429b5f31373f28256b64ecfb6f46e2a5b170458
Shukri Adams <mail@shukriadams.com>
2026-09-24 13:30:14 +0200

    Create ILoggger.cs

---
+++ src/Loggger/ILoggger.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Madscience.Logging
+{
+    public interface ILoggger : IDisposable
+    {
+        int VerbosityThreshold { get; set; }
+
+        void Dispose();
+
+        void Error(object source, string message); 
+
+        void Error(object source, object exception);
+
+        void Error(object source, string message, object exception);
+
+        void Warn(object source, string message);
+
+        void Warn(object source, object exception);
+
+        void Warn(string source, string message, object exception);
+
+        void Status(object source, string message, int verbosity = 0);
+
+        void Status(string source, string message, int verbosity = 0);
+
+        void Debug(object source, string message, int verbosity = 0);
+
+        void Debug(string source, string message, int verbosity = 0);
+        
+        void Trace(object source, string message, int verbosity = 0);
+
+        void Trace(string source, string message, int verbosity);
+    }
+}
